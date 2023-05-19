74a46c99d20924e6920e29f32a99fcaa836cf178
lorinesim <lorinesin02@gmail.com>
2023-05-19 20:03:06 +0300

    Initial Commit

---
+++ kampus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kampus
+//
+//  Created by lorin esim on 19.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
